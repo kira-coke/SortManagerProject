0a1663a763ea5de06e81aaf360fbc8f0f7b9ea41
kira-coke <kiracoke@outlook.com>
2022-06-29 19:04:19 +0100

    First basic tests

---
+++ src/test/java/BubbleSortTest.java
@@ -0,0 +1,20 @@
+import com.sparta.Kira.BubbleSort;
+import com.sparta.Kira.Sorter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BubbleSortTest {
+    Sorter bubbleSort;
+
+    @BeforeEach
+    void setUp(){bubbleSort = new BubbleSort();}
+
+    @Test
+    @DisplayName("Typical sort test")
+    void testSort(){
+        Assertions.assertEquals(4, (bubbleSort.sortArray(new int[]{4, 82, 9, 99, 11, 90, 67})[0]));
+        Assertions.assertNull(bubbleSort.sortArray(new int[]{}));
+    }
+}
